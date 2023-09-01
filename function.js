e2ed1de524226cf622c24ef1ba254c9042ee8d0b
BobbyHarish <harishkarnan004@gmail.com>
2023-09-01 22:23:17 +0530

    Function in JavaScript

---
+++ function.js
@@ -0,0 +1,13 @@
+//2.Function Declaration in JavaScript.
+let number1=10;
+let number2=20;
+function Airthematic(number1,number2){  //Argument Passing
+    let sum=number1+number2;
+    let mul=number1*number2;
+    let sub=number2-number1;
+    console.log("Addition:"+sum);
+    console.log("Subtraction:"+sub);
+    console.log("Multiplication:"+mul);
+
+}
+Airthematic(10,20)  //Arguments Value
