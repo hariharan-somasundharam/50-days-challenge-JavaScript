b255f2f56253922f6562c48a11d2845762ef72dd
BobbyHarish <harishkarnan004@gmail.com>
2023-09-17 11:51:29 +0530

    Finding GCD or HCF using JavaScript

---
+++ GCD.js
@@ -0,0 +1,15 @@
+// Finding GCD or HFC
+// Method : Conditional Statements & Looping Statements
+
+const num1 = prompt("Enter the First Integer:");
+const num2 = prompt("Enter the Second Integer:");
+
+for(let i=1;i<= num1 && i<= num2;i++)
+{
+    if( num1 % i==0 && num2 %i ==0)
+    {
+        gcd = i;
+    }
+}
+
+console.log(`GCD 0f ${num1} and ${num2} is ${gcd}.`);
